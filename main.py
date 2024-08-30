3ab007e7e5cff75273d11066d0d50ea86d5d60fe
vikasjalane <vikasjalane880@gmail.com>
2024-08-30 14:18:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def myself_vikas():
-    return "<p>I am a devops engineer from aws<p>"
+    return "<p>I am a devops engineer from India<p>"
 
 if __name__ == '__main__':
     app.run (host="0.0.0.0", port=2000)
